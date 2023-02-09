a3e0b2d5b122974fa1abeb92ecbb0500a66cb99e
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-02-09 17:04:27 +0100

    EDIT changed play button code

--- js/app.js
+++ js/app.js
@@ -3,26 +3,35 @@ const gridElement = document.querySelector('.grid')
 let sidelength = 10
 let cellNumber = sidelength ** 2
 
-for (let i = 0; i < cellNumber; i++) {
-    let num = i + 1
-    const cell = `<div class="cell" style="width: calc(100% / ${sidelength});">${num}</div>`
-    gridElement.innerHTML += cell
-}
 
 let playElement = document.querySelector('.play')
+let clicked = false
+
 playElement.addEventListener('click', function() {
-    gridElement.classList.add('grid-show')
-    playElement.classList.add('display-none')
+
+    if (clicked = true) {
+        gridElement.innerHTML = ''
+    }
+
+    for (let i = 0; i < cellNumber; i++) {
+        let num = i + 1
+        const cell = `<div class="cell" style="width: calc(100% / ${sidelength});">${num}</div>`
+        gridElement.innerHTML += cell
+    }
+    // gridElement.classList.add('grid-show')
+    // playElement.classList.add('display-none')
+    let cellElement = document.querySelectorAll('.cell')
+    console.log(typeof cellElement, cellElement)
+    
+    for (let i = 0; i < cellElement.length; i++) {
+        cellElement[i].addEventListener('click', function() {
+            console.log(i + 1)
+            cellElement[i].classList.toggle('clicked')
+        })
+    }
+    clicked = true
 })
+    
 
-let cellElement = document.querySelectorAll('.cell')
-console.log(typeof cellElement, cellElement)
-
-for (let i = 0; i < cellElement.length; i++) {
-    cellElement[i].addEventListener('click', function() {
-        console.log(i + 1)
-        cellElement[i].classList.toggle('clicked')
-    })
-}
 
 
